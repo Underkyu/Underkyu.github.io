b5b2b91418ef99053c5a2d1dc27f9fa30d710314
Miguel Yudi <migblad11@gmail.com>
2023-11-04 19:51:50 -0300

    Arrumando problema dos carrosseis

--- Criatil/JS/principal.js
+++ Criatil/JS/principal.js
@@ -1,10 +1,19 @@
-new Glider(document.querySelector('.glider'), {
-    slidesToScroll: 1,
+const gliders = document.querySelectorAll("[id^=glider]");
+
+gliders.forEach((glide) => {
+  const glider = document.querySelector(`#${glide.id} .glider`);
+  const gliderPrev = document.querySelector(`#${glide.id} .glider-prev`);
+  const gliderNext = document.querySelector(`#${glide.id} .glider-next`);
+  const gliderDots = document.querySelector(`#${glide.id} .dots`);
+
+  new Glider(glider, {
     slidesToShow: 4,
+    slidesToScroll: 4,
     draggable: true,
-    dots: '.dots',
+    dots: gliderDots,
     arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
+      prev: gliderPrev,
+      next: gliderNext
     }
   });
+});
